0cd21e3270d73a9b1a6f7f669f18d53ae2e7e8fa
ghazimoradi <ghazimoradi.amin@gmail.com>
2023-05-19 16:59:34 +0330

    final

--- app/src/main/java/com/ghores/dictionarycompose/home/presentation/HomeScreen.kt
+++ app/src/main/java/com/ghores/dictionarycompose/home/presentation/HomeScreen.kt
@@ -8,27 +8,23 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.compose.rememberNavController
 import com.ghores.dictionarycompose.R
 import com.ghores.dictionarycompose.common.UiEvents
 import com.ghores.dictionarycompose.home.data.remote.Meaning
@@ -40,7 +36,6 @@ import com.ghores.dictionarycompose.home.presentation.component.SearchTextFieldC
 import com.ghores.dictionarycompose.home.presentation.uistate.DefinitionUiState
 import com.ghores.dictionarycompose.ui.theme.DictionaryComposeTheme
 import com.ghores.dictionarycompose.ui.theme.SkyBlue
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import kotlinx.coroutines.flow.collectLatest
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -48,13 +43,13 @@ import kotlinx.coroutines.flow.collectLatest
 fun HomeScreen(
     definitionViewModel: DefinitionViewModel = hiltViewModel()
 ) {
-    //val scaffoldState = rememberScaffoldState()
+    val scaffoldState = rememberScaffoldState()
 
     LaunchedEffect(key1 = true) {
         definitionViewModel.eventFlow.collectLatest { event ->
             when (event) {
                 is UiEvents.SnackBarEvent -> {
-                    // scaffoldState.snackbarHostState.showSnackbar(event.message)
+                    scaffoldState.snackbarHostState.showSnackbar(event.message)
                 }
             }
         }
@@ -72,7 +67,7 @@ fun HomeScreen(
     DictionaryComposeTheme {
 
         Scaffold(
-            // scaffoldState = scaffoldState,
+            scaffoldState = scaffoldState,
             topBar = {
                 TopAppBar(
                     title = {
